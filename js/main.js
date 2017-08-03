d285e132bc859a923c25e2d205df09b70f9ebe80
Chadwicksavage <chadwick.joshua.savage@gmail.com>
2017-08-02 20:43:14 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let image = document.querySelector('#contact')
+image.onclick = toggleClass;
+
+function toggleClass(){
+	if (image.className = 'orange') {
+		image.className = 'dgrey'
+	} else {
+		image.className = 'orange'
+	}
+}
