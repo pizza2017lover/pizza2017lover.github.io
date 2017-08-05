986379420fdca68dfaa8e8bcda300bc04ed80fcc
Chadwicksavage <chadwick.joshua.savage@gmail.com>
2017-08-05 04:35:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,3 +10,17 @@ function toggleClass(){
 		poop.className = 'orange'
 	}
 }
+
+
+
+    function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
+    else document.getElementById('ac-wrapper').removeAttribute('style');
+}
+window.onload = function () {
+    setTimeout(function () {
+        PopUp('show');
+    }, 5000);
+}
+	
+
