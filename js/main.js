c6ba4aeea729156ccf6f6aba377cea4939d80af3
Chadwicksavage <chadwick.joshua.savage@gmail.com>
2017-08-05 04:39:55 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,8 +12,7 @@ function toggleClass(){
 }
 
 
-
-    function PopUp(hideOrshow) {
+function PopUp(hideOrshow) {
     if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
     else document.getElementById('ac-wrapper').removeAttribute('style');
 }
